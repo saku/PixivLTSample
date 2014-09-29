c6ebb320d2a026503dbc2904d17ec76b2dc6c27e
saku <saku2saku@gmail.com>
2014-09-30 01:21:32 +0900

    add Swift VC extends Objc class.

---
+++ PixivLTSample/extend/SwiftExtendObjcViewController.swift
@@ -0,0 +1,15 @@
+//
+//  SwiftExtendObjcViewController.swift
+//  PixivLTSample
+//
+//  Created by saku on 2014/09/30.
+//  Copyright (c) 2014年 Yoichiro Sakurai. All rights reserved.
+//
+
+import UIKit
+
+class SwiftExtendObjcViewController: BaseObjcViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
