1dba29ec97649719ce4caa928b9f367a04fe8233
saku <saku2saku@gmail.com>
2014-10-03 10:30:44 +0900

    add error sample obj-c class extends swift class

---
+++ PixivLTSample/extend/ObjcExtendSwiftViewController.h
@@ -0,0 +1,13 @@
+//
+//  ObjcExtendSwiftViewController.h
+//  PixivLTSample
+//
+//  Created by saku on 2014/10/03.
+//  Copyright (c) 2014年 Yoichiro Sakurai. All rights reserved.
+//
+
+#import "PixivLTSample-Swift.h"
+
+@interface ObjcExtendSwiftViewController : BaseSwiftViewController
+
+@end
---
+++ PixivLTSample/extend/ObjcExtendSwiftViewController.m
@@ -0,0 +1,37 @@
+//
+//  ObjcExtendSwiftViewController.m
+//  PixivLTSample
+//
+//  Created by saku on 2014/10/03.
+//  Copyright (c) 2014年 Yoichiro Sakurai. All rights reserved.
+//
+
+#import "ObjcExtendSwiftViewController.h"
+
+@interface ObjcExtendSwiftViewController ()
+
+@end
+
+@implementation ObjcExtendSwiftViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
